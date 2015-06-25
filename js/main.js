172c8de90a0d7fd138de8c2e7ae6e12091d6cbe8
Hao Chen <areigna@gmail.com>
2015-06-25 13:11:16 -0700

    phone rotation

--- js/main.js
+++ js/main.js
@@ -1,22 +1,30 @@
 var currentIndex = 0;
 function goTo (index, force) {
   if (!force && (index == null || index == currentIndex)) return;
-  $('#beaker'+currentIndex).fadeOut(function(){
+  $('#screen'+currentIndex).hide();
+  $('#beaker'+currentIndex).fadeOut(1000, function(){
     //light off the old dot
-    $('#beaker'+index).fadeIn(function(){
+    $('#beaker'+index).fadeIn(1000, function(){
       $('#nav' + currentIndex).removeClass('on');
       $('#nav' + index).addClass('on');
+      $('#screen'+index).show();
       currentIndex = index;
     });
+    //randomly rotate iphone
+    rotate( $('#iphone'), Math.random() * -80);
+  });
+}
+
+function rotate (ele, degree) {
+  var value = 'rotate(' + degree +'deg)';
+  console.log('rotating', value);
+  ele.css({
+    '-ms-transform': value,
+    '-webkit-transform': value,
+    'transform': value
   });
 }
 
 $(document).ready(function(){
   goTo(0, true);
-  //start auto transfering page
-  /*
-  setInterval(function(){
-    goTo(currentIndex+1);
-  }, 2000);
-  */
 });
